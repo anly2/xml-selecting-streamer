38acf8fc72a7ed52a9febe717d31dcd768bb5422
Anko Anchev <anko.anchev@gmail.com>
2017-07-13 20:53:17 +0300

    added a small "Main" to test if actually streaming

--- src/test/java/aanchev/xmlstreamer/AppTest.java
+++ src/test/java/aanchev/xmlstreamer/AppTest.java
@@ -1,7 +1,26 @@
 package aanchev.xmlstreamer;
 
+import java.io.File;
+import java.util.Scanner;
+
 import junit.framework.TestCase;
 
 public class AppTest extends TestCase {
 
+	public void testXMLStream() {
+		File file = new File("D:/Work/Java/nlp-data/elderscrolls_pages_current.xml");
+		AsyncXMLStreamer xmlstreamer = new AsyncXMLStreamer(file);
+
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+
+		xmlstreamer.on("page>title", e -> {
+//		xmlstreamer.on("page>title$~ns{0}", e -> {
+			System.out.println(e.getTag() + "{" + e.getText() + "}");
+			in.nextLine();
+		});
+
+		System.out.println("Reading: "+file);
+		xmlstreamer.drain();
+	}
 }
