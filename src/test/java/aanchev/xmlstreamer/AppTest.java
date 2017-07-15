af14a0e2c441f9d045991cfeebc8efa069459af0
Anko Anchev <anko.anchev@gmail.com>
2017-07-15 17:55:11 +0300

    added an "Eyeball" test

--- src/test/java/aanchev/xmlstreamer/AppTest.java
+++ src/test/java/aanchev/xmlstreamer/AppTest.java
@@ -7,20 +7,27 @@ import junit.framework.TestCase;
 
 public class AppTest extends TestCase {
 
-	public void testXMLStream() {
+	public void testOnData() {
 		File file = new File("D:/Work/Java/nlp-data/elderscrolls_pages_current.xml");
 		AsyncXMLStreamer xmlstreamer = new AsyncXMLStreamer(file);
 
 		@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
 
-		xmlstreamer.on("page>title", e -> {
-//		xmlstreamer.on("page>title$~ns{0}", e -> {
-			System.out.println(e.getTag() + "{" + e.getText() + "}");
-			in.nextLine();
+		int[] i = {0};
+		xmlstreamer.on("page$>title~ns{0}", e -> {
+			i[0]++;
+			String title = e.find("title").getText();
+			String text = e.find("text").getText();
+
+			System.out.println(" == " + title + " == ");
+			System.out.println(text);
+
+			in.nextLine(); //pause
 		});
 
 		System.out.println("Reading: "+file);
 		xmlstreamer.drain();
+		System.out.println(i[0] + " pages matched");
 	}
 }
